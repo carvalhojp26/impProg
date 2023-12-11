0e2ba6c69594d38cf1793f3bd6fa9409f193126c
Joao Paulo <acarvalho.jp@gmail.com>
2023-12-11 02:27:12 +0000

    typo

--- final-paper/main.c
+++ final-paper/main.c
@@ -2,24 +2,22 @@
 #include <stdio.h>
 
 int main() {
-    	const int MAX_PACIENTS = 10; // Adjust as needed
+    	const int MAX_PACIENTS = 10; 
     	Pacients pacients[MAX_PACIENTS];
     
    	readPacients("data/pacients.txt", pacients, MAX_PACIENTS);
 
     	int count = readPacients("data/pacients.txt", pacients, MAX_PACIENTS);
 
-    	// Print only the valid records
     	for (int i = 0; i < count; i++) {
         	printf("ID: %d, Nome: %s, Telemovel: %d\n", pacients[i].ID, pacients[i].name, pacients[i].phoneNumber);
     	}
 
-	const int MAX_DIETS = 10;  // Adjust as needed
+	const int MAX_DIETS = 10;
     	Diet diets[MAX_DIETS];
 
     	count = readDiet("data/diet.txt", diets, MAX_DIETS);
 
-    	// Print the Diet information
     	for (int i = 0; i < count; i++) {
         	printf("ID: %d, Date: %02d-%02d-%04d, Meal: %s, Food: %s, Calories: %d\n",
                		diets[i].ID, 
