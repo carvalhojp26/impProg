4504c0d71b023c795e91539ac7b7fa2972206cba
Joao Paulo <acarvalho.jp@gmail.com>
2023-12-11 01:55:13 +0000

    adding the readDiet function

--- final-paper/main.c
+++ final-paper/main.c
@@ -2,18 +2,31 @@
 #include <stdio.h>
 
 int main() {
-    const int MAX_PACIENTS = 10; // Adjust as needed
-    Pacients pacients[MAX_PACIENTS];
+    	const int MAX_PACIENTS = 10; // Adjust as needed
+    	Pacients pacients[MAX_PACIENTS];
     
-    readPacients("data/pacients.txt", pacients, MAX_PACIENTS);
+   	readPacients("data/pacients.txt", pacients, MAX_PACIENTS);
 
-    int count = readPacients("data/pacients.txt", pacients, MAX_PACIENTS);
+    	int count = readPacients("data/pacients.txt", pacients, MAX_PACIENTS);
 
-    // Print only the valid records
-    for (int i = 0; i < count; i++) {
-        printf("ID: %d, Nome: %s, Telemovel: %d\n", pacients[i].ID, pacients[i].name, pacients[i].phoneNumber);
-    }
+    	// Print only the valid records
+    	for (int i = 0; i < count; i++) {
+        	printf("ID: %d, Nome: %s, Telemovel: %d\n", pacients[i].ID, pacients[i].name, pacients[i].phoneNumber);
+    	}
 
-    return 0;
+	const int MAX_DIETS = 10;  // Adjust as needed
+    	Diet diets[MAX_DIETS];
+
+    	count = readDiet("data/diet.txt", diets, MAX_DIETS);
+
+    	// Print the Diet information
+    	for (int i = 0; i < count; i++) {
+        	printf("ID: %d, Date: %02d-%02d-%04d, Meal: %s, Food: %s, Calories: %d\n",
+               		diets[i].ID, 
+               		diets[i].date.day, diets[i].date.month, diets[i].date.year, 
+               		diets[i].meal, diets[i].food, 
+               		diets[i].calories);
+    	}
+    	return 0;
 }
 
--- final-paper/utils.c
+++ final-paper/utils.c
@@ -5,15 +5,33 @@ int readPacients(char path[], Pacients *pacients, int max_size) {
 	FILE *file = fopen(path, "r");
 	if(file == NULL) {
 		printf("Nao foi possivel abrir o arquivo.\n");
-		return;
+		return 0;
 	}
 	
 	char line[100];
 	int i;
-	for(i=0;i<max_size && fgets(line, sizeof(line), file); i++) {
+	for(i=0; i<max_size && fgets(line, sizeof(line), file); i++) {
 		sscanf(line, "%d;%49[^;];%d", &pacients[i].ID, pacients[i].name, &pacients[i].phoneNumber);
 	};
 	
 	fclose(file);
 	return i;	
 }
+
+int readDiet(char path[], Diet *diet, int max_size) {
+	FILE *file = fopen(path, "r");
+	if(file == NULL) {
+		printf("Nao foi possivel abrir o arquivo.\n");
+		return 0;
+	}
+
+	char line[100];
+	int i;
+	for(i=0; i<max_size && fgets(line, sizeof(line), file); i++) {
+		sscanf(line, "%d;%d-%d-%d;%49[^;];%49[^;];%d", &diet[i].ID, &diet[i].date.day, &diet[i].date.month, &diet[i].date.year, diet[i].meal, diet[i].food, &diet[i].calories);
+	};
+
+	fclose(file);
+	return i;
+};
+
