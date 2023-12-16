39b6da25099ff7d4055846980e6c6275d8e55230
Joao Paulo <acarvalho.jp@gmail.com>
2023-12-16 17:14:23 +0000

    Revert "Indenting files"
    This reverts commit e67a3cac6d933284795974e4ad72dd91f460c307.

--- final-paper/main.c
+++ final-paper/main.c
@@ -1,41 +1,34 @@
 #include "utils.h"
 #include <stdio.h>
 
-int
-main ()
-{
-  const int MAX_SIZE = 10;
+int main() {
+    const int MAX_SIZE = 10;
 
-  Patients patients[MAX_SIZE];
-  int numPatients =
-    readFile ("data/patients.txt", patients, MAX_SIZE, PATIENTS);
-  for (int i = 0; i < numPatients; i++)
-    {
-      printf ("ID: %d, Nome: %s, Telemovel: %d\n", patients[i].ID,
-	      patients[i].name, patients[i].phoneNumber);
+    Patients patients[MAX_SIZE];
+    int numPatients = readFile("data/patients.txt", patients, MAX_SIZE, PATIENTS);
+    for (int i = 0; i < numPatients; i++) {
+        printf("ID: %d, Nome: %s, Telemovel: %d\n", patients[i].ID, patients[i].name, patients[i].phoneNumber);
     }
 
-  Diet diets[MAX_SIZE];
-  int numDiets = readFile ("data/diet.txt", diets, MAX_SIZE, DIET);
-  for (int i = 0; i < numDiets; i++)
-    {
-      printf
-	("ID: %d, Date: %02d-%02d-%04d, Meal: %s, Food: %s, Calories: %d\n",
-	 diets[i].ID, diets[i].date.day, diets[i].date.month,
-	 diets[i].date.year, diets[i].meal, diets[i].food, diets[i].calories);
+    Diet diets[MAX_SIZE];
+    int numDiets = readFile("data/diet.txt", diets, MAX_SIZE, DIET);
+    for (int i = 0; i < numDiets; i++) {
+        printf("ID: %d, Date: %02d-%02d-%04d, Meal: %s, Food: %s, Calories: %d\n",
+                diets[i].ID,
+                diets[i].date.day, diets[i].date.month, diets[i].date.year,
+                diets[i].meal, diets[i].food,
+                diets[i].calories);
     }
 
-  MealPlan mealPlans[MAX_SIZE];
-  int numMealPlans =
-    readFile ("data/mealPlan.txt", mealPlans, MAX_SIZE, MEAL_PLAN);
-  for (int i = 0; i < numMealPlans; i++)
-    {
-      printf
-	("ID: %d, Date: %02d-%02d-%04d, Meal: %s, MinCal: %d, MaxCal: %d\n",
-	 mealPlans[i].ID, mealPlans[i].date.day, mealPlans[i].date.month,
-	 mealPlans[i].date.year, mealPlans[i].meal, mealPlans[i].minCal,
-	 mealPlans[i].maxCal);
+    MealPlan mealPlans[MAX_SIZE];
+    int numMealPlans = readFile("data/mealPlan.txt", mealPlans, MAX_SIZE, MEAL_PLAN);
+    for (int i = 0; i < numMealPlans; i++) {
+        printf("ID: %d, Date: %02d-%02d-%04d, Meal: %s, MinCal: %d, MaxCal: %d\n",
+                mealPlans[i].ID,
+                mealPlans[i].date.day, mealPlans[i].date.month, mealPlans[i].date.year,
+                mealPlans[i].meal, mealPlans[i].minCal, mealPlans[i].maxCal);
     }
 
-  return 0;
+    return 0;
 }
+
--- final-paper/utils.h
+++ final-paper/utils.h
@@ -1,42 +1,37 @@
 //DataTypes
-typedef struct
-{
-  int day;
-  int month;
-  int year;
+typedef struct {
+    	int day;
+    	int month;
+    	int year;
 } Date;
 
-typedef struct
-{
-  int ID;
-  char name[50];
-  int phoneNumber;
+typedef struct {
+	int ID;
+	char name[50];
+	int phoneNumber;
 } Patients;
 
-typedef struct
-{
-  int ID;
-  Date date;
-  char meal[50];
-  char food[50];
-  int calories;
+typedef struct {
+	int ID;
+	Date date;
+	char meal[50];
+	char food[50];
+	int calories;
 } Diet;
 
-typedef struct
-{
-  int ID;
-  Date date;
-  char meal[50];
-  int minCal;
-  int maxCal;
+typedef struct {
+	int ID;
+	Date date;
+	char meal[50];
+	int minCal;
+	int maxCal;
 } MealPlan;
 
-typedef enum
-{
-  PATIENTS,
-  DIET,
-  MEAL_PLAN
+typedef enum {
+    PATIENTS,
+    DIET,
+    MEAL_PLAN
 } FileType;
 
 //Funcoes 
-int readFile (char *path, void *data, int max_size, FileType fileType);
+int readFile(char *path, void *data, int max_size, FileType fileType);
